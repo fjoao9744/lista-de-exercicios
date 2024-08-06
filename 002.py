7248ebf2c8e4f456ae5fb063490b309328282a69
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-08-06 12:48:00 -0300

    Create 002.py

---
+++ 002.py
@@ -0,0 +1,16 @@
+'''print(1 + 1)'''
+
+'''n1 = int(input())
+n2 = int(input())
+
+print(n1 + n2)'''
+
+
+#soma itens de uma lista ou tupla(sum)
+'''n = (int(input()), int(input()))
+print(sum(n))'''
+
+def soma(a, b):
+    return a + b
+
+print(soma(int(input()), int(input())))
