039c6320adcda8718d0c748dc575191b7516779d
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-08-14 19:37:08 -0300

    Update 009.py

--- resolução-python/009.py
+++ resolução-python/009.py
@@ -9,7 +9,7 @@ print(f"você tem {dol:.2f} dolares e {euro:.2f} euros") #arredondado
 
 '''
 
-
+'''
 #não é bem a mesma coisa mas isso é um teste de API
 import requests
 import json
@@ -20,4 +20,19 @@ dolar = json.loads(var.content)
 
 print(f"o {dolar['USDBRL']['name']} esta com a cotação de {dolar['USDBRL']['high']}")
 
+'''
 
+import requests
+import json
+
+def dollar_convert(x):
+    var = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL')
+    moeda = json.loads(var.content)
+    
+    dolar = moeda['USDBRL']['high']
+    
+    return x * float(dolar)
+
+print('-' * 10, 'CONVERSOR DE REAL PARA DOLAR', '-' * 10)
+n = int(input('quantos reais deseja converter: '))
+print(f'o valor convertido fica: {dollar_convert(n)}')
