7e75e100402ffbd4b287e77d621e006f5cebdb16
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-08-26 18:51:26 -0300

    Create 014.py

---
+++ resolução-python/014.py
@@ -0,0 +1,10 @@
+n = input('digite seu nome completo: ').strip()
+
+print(f'seu primeiro nome é {n.split()[0]}')
+print(f'a primeira letra do seu nome é {n[0]}')
+print(f'seu nome todo em maiusculo é {n.upper()}')
+print(f'e seu nome tem {len(n)} caracteres')
+
+
+
+
