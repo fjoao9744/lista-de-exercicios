ba155445cdc86008c447527ea08a0b7b458a6879
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-08-27 18:43:02 -0300

    Update 014.py

--- resolução-python/014.py
+++ resolução-python/014.py
@@ -1,10 +1,19 @@
+'''
 n = input('digite seu nome completo: ').strip()
 
 print(f'seu primeiro nome é {n.split()[0]}')
 print(f'a primeira letra do seu nome é {n[0]}')
 print(f'seu nome todo em maiusculo é {n.upper()}')
 print(f'e seu nome tem {len(n)} caracteres')
+'''
+def primeiro_nome(x):
+    return str(x).split()[0]
 
+n = input('digite seu nome completo: ').strip()
 
+print(f'seu primeiro nome é {primeiro_nome(n)}')
+print(f'a primeira letra do seu nome é {n[0]}')
+print(f'seu nome todo em maiusculo é {n.upper()}')
+print(f'e seu nome tem {len(n)} caracteres')
 
 
